c40e13aa1b66d212383049d3b61c324b20b93899
“Josue <ijosue.carreon@gmail.com>
2023-02-21 10:01:24 -0600

    Feb 22 morning

--- src/components/Query.js
+++ src/components/Query.js
@@ -1,16 +1,20 @@
-
+import { useState } from "react";
 
 const Query =() =>{
+    const [inputName, setInputName] = useState('')
+    const [inputBreed, setInputBreed] = useState('')
 
     return(
             <div className="Query">
                 <span>Name: </span>
-                <input type="text"></input>
+                <input value= { inputName } type="text"></input>
                 <span>Breed: </span>
-                <input type="text"></input>
+                <input value= { inputBreed }type="text"></input>
                 <button>Submit</button>
             </div>
     )
 }
 
 export default Query;
+
+// add key={index}
